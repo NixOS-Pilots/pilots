0454745e8b154ced3ab734e10a7216a98e5a2587
kev <31861128+yqlbu@users.noreply.github.com>
2024-03-22 23:38:14 +0800

    feat(flake): bootstrap project, add pre-commit-hooks (#2)
    * ci: add justfile
    
    * feat: initialize flake
    
    * feat(flake): integrate pre-commit-hooks

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix flake for bleeding-edge and unreleased open-source packages.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }@inputs: with inputs;
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; config.allowUnfree = true; };
+        genChecks = system: (pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true; # formatter
+            statix.enable = true; # linter
+            deadnix.enable = true; # linter
+          };
+        });
+      in
+      {
+        # checks
+        checks.pre-commit-check = genChecks system;
+        packages = { };
+      });
+}
