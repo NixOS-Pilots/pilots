9c0a36121e2e8ff861f877c0302380c1973920ed
kev <yqlbu@bu.edu>
2024-03-22 23:36:42 +0800

    feat(flake): integrate pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -2,19 +2,30 @@
   description = "Nix flake for bleeding-edge and unreleased open-source packages.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { nixpkgs, flake-utils, ... }@inputs: with inputs;
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
     in
-    (flake-utils.lib.eachSystem supportedSystems (system:
+    flake-utils.lib.eachSystem supportedSystems (system:
       let
         pkgs = (import nixpkgs) { inherit system; config.allowUnfree = true; };
+        genChecks = system: (pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true; # formatter
+            statix.enable = true; # linter
+            deadnix.enable = true; # linter
+          };
+        });
       in
       {
+        # checks
+        checks.pre-commit-check = genChecks system;
         packages = { };
-      }));
+      });
 }
