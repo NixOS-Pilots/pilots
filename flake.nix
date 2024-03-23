84c1a964df6418b1e132fe30e369b9a66121b9fc
kev <yqlbu@bu.edu>
2024-03-23 10:02:56 +0800

    feat(flake): add inputs.nix-fast-build

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nix-fast-build.url = "github:Mic92/nix-fast-build";
   };
 
   outputs = { nixpkgs, flake-utils, ... }@inputs: with inputs;
@@ -27,6 +28,7 @@
         # checks
         checks.pre-commit-check = genChecks system;
         packages = {
+          nix-fast-build = inputs.nix-fast-build.packages.${system}.default;
           glider = pkgs.callPackage ./pkgs/glider { };
         };
       });
