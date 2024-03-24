5e21d8081da98b60b73b644a5fcbc1d5e522d3e2
kev <yqlbu@bu.edu>
2024-03-24 19:32:29 +0800

    patch(pkgs/neovim): use nix-community/neovim-nightly-overlay as upstream

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -5,19 +5,20 @@
 
 stdenv.mkDerivation (finalAttrs: {
   name = "neovim";
+  version = "unstable-2024-03-24-ed91060";
   src = (sharedLib.legacyGetFlake
     {
       src = fetchFromGitHub
         {
-          owner = finalAttrs.name;
-          repo = finalAttrs.name;
-          rev = "ed910604ca677c897f003d00832dc6a1cb3ac65d";
-          hash = "sha256-+vfgzQFEPenQmhRl3aQ39KiU+92GVhO/4i05MyUQBOU=";
-        } + "/contrib";
+          owner = "nix-community";
+          repo = "neovim-nightly-overlay";
+          rev = "dc1d09c95137ce5b6889f4266ea7301d2af071f1";
+          hash = "sha256-+RQ20E5N6bQcZtSuwIx+4ELFwbfMOZ7W7tgrU9vxdA8=";
+        };
     }
   ).packages.x86_64-linux.neovim;
 
   installPhase = ''
-    install -D $src/bin/${finalAttrs.name} $out/bin/${finalAttrs.name}
+    cp -r . $out/
   '';
 })
